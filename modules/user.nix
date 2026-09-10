250204d46adf39de695a08d7dc27f290e9aa0512
Mads Johannessen <9995017+madsjoh@users.noreply.github.com>
2026-09-10 13:09:09 +0200

    feat: disable jdtls lsp (#23)

--- modules/user.nix
+++ modules/user.nix
@@ -111,10 +111,6 @@ in
         config = {
           lsp = {
             jdtls.disabled = true;
-            java = {
-              command = [ "jdtls" "-data" "/tmp/jdtls-workspace" "-Djava.import.generatesMetadataFilesAtProjectRoot=false" ];
-              extensions = [ ".java" ];
-            };
             csharp.command = [ "csharp-ls" ];
             fish = {
               command = [ "fish-lsp" "start" ];
